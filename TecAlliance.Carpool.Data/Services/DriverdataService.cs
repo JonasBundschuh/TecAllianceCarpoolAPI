ef357ad6d1862df5eabea50af9b582e8c0b473ba
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-10-18 13:49:03 +0200

    General Bug Fixes | Got first run working | Sort of folders | Changed Paths

--- TecAlliance.Carpool.Data/Services/DriverdataService.cs
+++ TecAlliance.Carpool.Data/Services/DriverdataService.cs
@@ -6,12 +6,12 @@
         public void AddNewDriver(string freeSeats, string smoke, string fullName, string startLoc, string endLoc, string timeStart, string timeEnd)
         {
 
-            foreach (string file in Directory.EnumerateFiles("C:\\Projects\\FahrgemeinschaftsAPP\\bin\\Fahrgemeinschaften", "*.csv"))
+            foreach (string file in Directory.EnumerateFiles("C:\\001\\012TecAllianceCarpoolAPI\\Bin\\Carpools", "*.csv"))
             {
                 Id++;
 
             }
-            File.AppendAllText($"C:\\Projects\\FahrgemeinschaftsAPP\\bin\\Fahrgemeinschaften\\Fahrgemeinschaft{Id}.csv", $"{Id};{freeSeats};{smoke};{fullName};{startLoc};{endLoc};{timeStart};{timeEnd};\n");
+            File.AppendAllText($"C:\\001\\012TecAllianceCarpoolAPI\\Bin\\Carpools\\Fahrgemeinschaft{Id}.csv", $"{Id};{freeSeats};{smoke};{fullName};{startLoc};{endLoc};{timeStart};{timeEnd};\n");
 
         }
     }
